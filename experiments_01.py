916d533793ce82fd96f2569a71630f9cb1378da1
Colin Stephen <colin.stephen@gmail.com>
2020-06-16 17:07:24 +0100

    use chunking to our advantage; also save to the results file incrementally

--- experiments_01.py
+++ experiments_01.py
@@ -18,11 +18,21 @@ of HVG construction using different algorithms.
 
 
 
+TESTING = True
+data_filename = 'experiments_01_data.pickle'
+results_filename = r'experiments_01_results.csv'
+
+
+
 import csv
 import time
+import pickle
 import numpy as np
+import multiprocessing as mp
+
 from functools import partial
-import multiprocessing
+from datetime import datetime
+now = datetime.now
 
 import streams
 from bst_hvg import hvg as binary_search_hvg
@@ -33,7 +43,10 @@ import sys
 sys.setrecursionlimit(25000)  # needed for DC method
 
 
-# Set up the time series data sources
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Generate the time series data
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 sources = {
 	'random': streams.random,
@@ -41,6 +54,7 @@ sources = {
 	'logistic': streams.logistic_attractor,
 	'standard': streams.standard_map
 }
+
 
 hursts = np.linspace(0.15, 0.85, num=15)
 for hurst in hursts:
@@ -57,95 +71,127 @@ for tmax in tmaxs:
 	sources[f'rossler_{tmax}'] = partial(streams.rossler_attractor, tmax=tmax)
 
 
-
-# Running the experiments
-
-def time_algorithm(alg, x):
-	'''
-	Apply algorithm to time series x and return the elapsed process time.
-	Return -1 when the algorithm crashes, usually due to too much recursion.
-	'''
-	t0 = time.process_time()
-	try:
-		_ = alg(x)
-		t1 = time.process_time()
-	except Exception as e:
-		t1 = t0 - 1
-	return t1 - t0
-
-
-def time_algorithms(x):
-
-	t0 = time_algorithm(binary_search_hvg, x)
-	t1 = time_algorithm(divide_conquer_hvg, x)
-	t2 = time_algorithm(dual_tree_hvg, x)
-
-	return t0, t1, t2
-
-
-
-def single_run(args):
-
-		source, length, rep = args
-		tries = 0
-		x = None
-		success = False
-
-		print(f'source : {source} - length : {length} - rep : {rep}')
-
-		while not success and tries < 10:
-			try:
-				x = sources[source](length)
-				success = True
-			except Exception as e:
-				tries += 1
-
-		if x is None:
-			print(f'FAILED with source {source} length {length} rep {rep}')
-			t0, t1, t2 = -1, -1, -1
-		else:
-			t0, t1, t2 = time_algorithms(x)
-
-		# writer.writerow([source, length] + [t0, t1, t2])
-
-		return [source, length, t0, t1, t2]
-
-
-
-# Set up the experimental parameters
-
-TESTING = True
-
 if TESTING:
-	reps = range(2)
 	lengths = [2**x for x in range(8,10)]
-	results_file = r'results_01.TESTING.csv'
+	reps = range(3)
 else:
-	reps = range(10)
-	lengths = [2**x for x in range(8,18)]
-	results_file = r'results_01.csv'
-
-csv_headers = ['source_name', 'length', 'bst_time', 'dc_time', 'dt_time']
+	lengths = [2**x for x in range(8,17)]
+	reps = range(11)
 
 
-
-# Run the experiments
-
-pool = multiprocessing.Pool()
-params = [[source, length, rep] for source in sources for length in
-	lengths for rep in reps]
-results = pool.map(single_run, params)
+data_keys = [(source, length, rep) for source in sources for length in lengths
+	for rep in reps]
 
 
+def generate_time_series(data_key):
+	source, length, rep = data_key  # can ignore rep
 
-# Write the results
+	# some sources may diverge, overflow, or exceed machine precision
+	# so try multiple times to generate a sequence that works 
 
-with open(results_file, 'w') as f:
+	tries_remaining = 10
+	time_series = None
+
+	while time_series is None and tries_remaining > 0:
+		try:
+			time_series = sources[source](length)
+		except Exception as e:
+			tries -= 1
+
+	return time_series
+
+
+print(f'Begin generating experimental data at {now()}')
+
+
+pool = mp.Pool()
+time_series_data = pool.imap(generate_time_series, data_keys)
+pool.close()
+pool.join()
+
+
+data = {}
+for data_key in data_keys:
+	time_series = time_series_data.next()
+	if time_series is None:
+		print(f'generating data with parameters {data_key} failed')
+	else:
+		data[data_key] = time_series
+pickle.dump(data, open(data_filename, 'wb'))
+
+
+print(f'Saved experimental data to {data_filename} at {now()}')
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Run the algorithms on the experimental data
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+algs = {
+	'dual_tree_hvg': dual_tree_hvg,
+	'binary_search_hvg': binary_search_hvg,
+	'divide_conquer_hvg': divide_conquer_hvg
+}
+
+
+exp_keys = [(alg, data_key) for data_key in data_keys for alg in algs]
+
+
+def time_algorithm(experiment_params):
+	'''
+	Apply an HVG algorithm to data and return the elapsed thread/process time.
+
+	To avoid reporting spurious process delays return the minimum of
+	several trials.
+	'''
+
+	alg, data_key = experiment_params
+	hvg_algorithm = algs[alg]
+	time_series = data[data_key]
+
+	try:
+		t0 = time.perf_counter()
+		_ = hvg_algorithm(time_series)
+		result = time.perf_counter() - t0
+	except Exception as e:
+		# Return -1 when an algorithm crashes
+		# (usually due to too much recursion).
+		result = -1
+
+	return result
+
+
+print(f'Begin running experiments at {now()}')
+
+
+pool = mp.Pool(3)
+runtime_results = pool.imap(time_algorithm, exp_keys, chunksize=1)
+
+
+with open(results_filename, 'w') as f:
 
 	writer = csv.writer(f)
+	csv_headers = ['source', 'length', 'rep', 'hvg_algorithm', 'time_in_seconds']
 	writer.writerow(csv_headers)
 
-	for result in results:
-		writer.writerow(result)
+	completed = 0
+	total = len(exp_keys)
+
+	for alg, (source, length, rep) in exp_keys:
+
+		runtime_result = runtime_results.next()
+		writer.writerow([source, length, rep, alg, runtime_result])
+
+		completed += 1
+		if completed % 50 == 0:
+			print(f'Completed {completed} of {total} at {now()}')
+
+	print(f'Completed saving results to {results_filename} at {now()}')
+
+
+pool.close()
+pool.join()
+
 
 
