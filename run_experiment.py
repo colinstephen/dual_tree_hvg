c3ccf2501155a2a6d7f67926228643061ac8db9e
Colin Stephen <colin.stephen@gmail.com>
2020-06-22 00:37:23 +0100

    lots of refactoring again, for parallelism

--- run_experiment.py
+++ run_experiment.py
@@ -43,53 +43,86 @@ from multiprocessing import Pool
 
 
 
-def generate_time_series(data_key, sources):
+def generate_time_series(experiment_data):
 	'''
 	Uses a source stream name and a time series length specified in `data_key`
 	to generate a time series. A dictionary of sources `sources` provides access
 	to the actual source function.
+
+	The argument `experiment_data` is a dict:
+		{
+			'algorithm': {'name': alg_name, 'func': alg_func},
+			'data': {'source': {'name': source_name, 'func': source_func}
+					 'length': length,
+					 'rep': rep,
+					 'time_series': None},
+			'result': None
+		}
+	
+	The job of this function is to populate the value for
+	experiment_data['data']['time_series'] as a sequence of values.
+
+	Some sources may diverge, overflow, or exceed machine precision
+	so try multiple times to generate a sequence that works 
+	and return None if it fails every time.
 	'''
+	time_series_data = experiment_data['data']
 
-	source, length, rep = data_key  # NB: can ignore rep
-
-	# Some sources may diverge, overflow, or exceed machine precision
-	# so try multiple times to generate a sequence that works 
-	# and return None if it fails every time.
+	func = time_series_data['source']['func']
+	length = time_series_data['length']
 
 	tries_remaining = 10
 	time_series = None
 
 	while time_series is None and tries_remaining > 0:
 		try:
-			time_series = sources[source](length)
+			time_series = func(length)
 		except Exception as e:
 			tries -= 1
 
-	return data_key, time_series
+	return time_series
+
+	# time_series_data['time_series'] = time_series
+
+	# return time_series_data
 
 
 
-def time_algorithm(experiment_params, dict_of_algs, dict_of_time_series):
+def time_algorithm(experiment_data):
 	'''
 	Apply an HVG algorithm to data using one of the algs
 	and return the elapsed thread/process time.
+	
+	The argument `experiment_data` is a dict:
+		{
+			'algorithm': {'name': alg_name, 'func': alg_func},
+			'data': {'source': {'name': source_name, 'func': source_func}
+					 'length': length,
+					 'rep': rep,
+					 'time_series': sequence_of_values},
+			'result': None
+		}
+	
+	The job of this function is to populate the value for
+	experiment_data['result'] as a duration in seconds.
 	'''
-
-	alg, data_key = experiment_params
-	hvg_algorithm = dict_of_algs[alg]
-	time_series = dict_of_time_series[data_key]
+	func = experiment_data['algorithm']['func']
+	time_series = experiment_data['data']['time_series']
 
 	try:
 		t0 = time.perf_counter()
-		_ = hvg_algorithm(time_series)
+		_ = func(time_series)
 		result = time.perf_counter() - t0
 	except Exception as e:
-		raise(e)
 		# Return -1 when an algorithm crashes
 		# (usually due to too much recursion).
 		result = -1
 
-	return experiment_params, result
+	return result
+
+	# experiment_data['result'] = result
+
+	# return experiment_data
 
 
 
@@ -168,7 +201,7 @@ def get_experiment(EXPERIMENT, TESTING=True):
 				amplitude=amp)
 
 		if TESTING:
-			lengths = [2**8]
+			lengths = [2**12]
 			reps = range(2)
 		else:
 			lengths = [2**17]
@@ -183,99 +216,153 @@ def get_experiment(EXPERIMENT, TESTING=True):
 	assert EXPERIMENT in [1, 2]
 
 
-	data_filename = f'experiment_{EXPERIMENT}{"_TESTING" if TESTING else ""}_data.pickle'
 	results_filename = f'experiment_{EXPERIMENT}{"_TESTING" if TESTING else ""}_results.csv'
 
 
-	return {
-		'sources': sources,
-		'lengths': lengths,
-		'reps': reps,
-		'algs': algs,
-		'data_filename': data_filename,
-		'results_filename': results_filename
-	}
+	experimental_data = []
+
+	'''
+	Now build a list of dictionaries of the following form:
+		{
+			'algorithm': {'name': alg_name, 'func': alg_func},
+			'data': {'source': {'name': source_name, 'func': source_func},
+					 'length': length,
+					 'rep': rep,
+					 'time_series': None},
+			'result': None
+		}
+	'''
+
+	for alg in algs:
+		algorithm = {'name':alg, 'func':algs[alg]}
+		for source in sources:
+			data_source = {'name':source, 'func':sources[source]}
+			for length in lengths:
+				for rep in reps:
+					experiment = {
+						'algorithm': algorithm,
+						'data': {'source': data_source,
+								 'length': length,
+								 'rep': rep,
+								 'time_series': None},
+						'result': None
+					}
+					experimental_data.append(experiment)
+
+	return experimental_data
+
+
+def get_data_filename(EXPERIMENT, TESTING=True):
+	return f'experiment_{EXPERIMENT}{"_TESTING" if TESTING else ""}_data.pickle'
+
+
+
+def get_results_filename(EXPERIMENT, TESTING=True):
+	return f'experiment_{EXPERIMENT}{"_TESTING" if TESTING else ""}_results.csv'
+
+
+
+def save_result(result, file_handle):
+	writer = csv.writer(file_handle)
+	source = result['data']['source']['name']
+	length = result['data']['length']
+	rep = result['data']['rep']
+	alg = result['algorithm']['name']
+	result = result['result']
+	writer.writerow([source, length, rep, alg, result])
+	return None
+
+
+
+def get_results_file(file_name):
+	'''
+	Get a file handle for a new results CSV file and populate the header row.
+	'''
+
+	f = open(file_name, 'w')
+	writer = csv.writer(f)
+	writer.writerow(['source', 'length', 'rep', 'hvg_algorithm',
+		'time_in_seconds'])
+	return f
 
 
 
 def run_experiment(EXPERIMENT, TESTING=True):
+	'''
+	The main method of the script. Given an experiment number go through the
+	following processes:
+
+	* Generate the experimental parameters - algorithms, sequence lengths, etc.
+	* Use the experiment parameters to generate time series data
+		- runs in parallel
+		- pickles the generated data to disk
+	* Process the time series data with the selected algorithms and time them
+		- runs in parallel
+		- writes timing results to a CSV file as it goes
+	'''
+
+	data_filename = get_data_filename(EXPERIMENT, TESTING)
+	results_filename = get_results_filename(EXPERIMENT, TESTING)
+
 
 	print(f'Begin running experiment {EXPERIMENT}: {now()}')
-	
-	experiment = get_experiment(EXPERIMENT, TESTING=TESTING)
+	experimental_data = get_experiment(EXPERIMENT, TESTING=TESTING)
 
-	sources = experiment['sources']
-	lengths = experiment['lengths']
-	reps = experiment['reps']
-	algs = experiment['algs']
-	data_filename = experiment['data_filename']
-	results_filename = experiment['results_filename']
 
-	# ~~~~~~~~~~~~~~~~~
-	# GENERATE THE DATA
-	# ~~~~~~~~~~~~~~~~~
-
-	data = {}
-	data_keys = [(source, length, rep) for source in sources for length in
-		lengths for rep in reps]
+	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	# Generate the time series data
+	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 	print(f'\tBegin generating experimental data: {now()}')
 
-	pool = Pool()
-	time_series_data = pool.starmap(generate_time_series, zip(data_keys,
-		repeat(sources)), chunksize=1)
-	for data_key, time_series in time_series_data:
-		if time_series is None:
-			print(f'\t\tgenerating data with parameters {data_key} failed')
-		else:
-			data[data_key] = time_series
-
-	pickle.dump(data, open(data_filename, 'wb'))
-
-	print(f'\tSaved experimental data to {data_filename}: {now()}')
-
-	pool.close()
-	pool.join()
-	pool.terminate()
+	with Pool() as pool:
+		
+		results = pool.imap_unordered(generate_time_series, experimental_data)
+		
+		for exp in experimental_data:
+			exp['data']['time_series'] = results.next()
+		
+		pool.close()
+		pool.join()
+	
+	print(f'\tEnd generating experimental data: {now()}')
+	
+	pickle.dump(experimental_data, open(data_filename, 'wb'))
+	print(f'\tSaved time series data to {data_filename}: {now()}')
 
 
+	
+	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	# Run the algorithms on the experimental data and save run times
+	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	# Run the algorithms on the experimental data
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	results_file = get_results_file(results_filename)
+	
+	with Pool() as pool:
+		print(f'\tBegin running experiments: {now()}')
+		
+		completed = 0
+		total = len(experimental_data)
+		
+		results = pool.imap_unordered(time_algorithm, experimental_data)
+		
+		for exp in experimental_data:
+			result = results.next()
+			exp['result'] = result
+			save_result(exp, results_file)
 
-	exp_keys = [(alg, data_key) for data_key in data_keys for alg in algs]
+			completed += 1
+			if completed % 50 == 0:
+				print(f'\t\tCompleted {completed} of {total}: {now()}')
 
-	print(f'\tBegin running experiments: {now()}')
+		pool.close()
+		pool.join()
 
-	pool = Pool()
-	runtime_results = pool.starmap(time_algorithm, zip(exp_keys, repeat(algs),
-		repeat(data)), chunksize=1)
+	results_file.close()
+	print(f'\tSaved results to {results_filename}: {now()}')
 
-	f = open(results_filename, 'w')
-	writer = csv.writer(f)
-	csv_headers = ['source', 'length', 'rep', 'hvg_algorithm',
-		'time_in_seconds']
-	writer.writerow(csv_headers)
-
-	completed = 0
-	total = len(exp_keys)
-
-	for (alg, (source, length, rep)), result in runtime_results:
-
-		writer.writerow([source, length, rep, alg, result])
-
-		completed += 1
-		if completed % 50 == 0:
-			print(f'\t\tCompleted {completed} of {total} at {now()}')
-
-	f.close()
-
-	print(f'\tCompleted saving results to {results_filename}: {now()}')
-
-	pool.close()
-	pool.join()
-	pool.terminate()
+	pickle.dump(experimental_data, open(data_filename, 'wb'))
+	print(f'\tSaved all experiments and results to {data_filename}: {now()}')
 
 	print(f'Completed running experiment {EXPERIMENT}: {now()}')
 
